edb52f782f4e8e3635ee7472cb9b548b07563b60
stephandeckers <stephandeckers@hotmail.com>
2022-07-22 20:54:45 +0200

    b

--- PXS.IfRF.Data/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle		( "PXS.IfRF.Data" )]
-[assembly: AssemblyDescription	( "PXS.IfRF.Data" )]
-[assembly: AssemblyProduct		( "PXS.IfRF.Data" )]
--- PXS.IfRF.Logging/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle		( "PXS.IfRF.Logging" )]
-[assembly: AssemblyDescription	( "PXS.IfRF.Logging" )]
-[assembly: AssemblyProduct		( "PXS.IfRF.Logging" )]
